85d07746256d1139fa3c2b07787db0ab723f1efc
wasingsd <wasin.gsd@gmail.com>
2025-12-27 15:45:30 +0700

    Fix: Add assetPrefix to load images correctly on GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: process.env.NODE_ENV === 'production' ? '/prgbgroup' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/prgbgroup' : '',
   images: {
     unoptimized: true,
   },
